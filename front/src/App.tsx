9f4e097980519e7aad8a8f3789a8d0cb1e51a16d
Gustavo Bañares de Bacco <gustavobbacco@gmail.com>
2025-04-25 19:17:42 -0300

    refactor: import landing page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,8 +5,8 @@ import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AppLayout } from './layout/app'
 import { SignUp } from './pages/auth/sign-up'
-import { LandingPage } from './pages/app/landing-page'
 import CVManager from './pages/app/cv-manager'
+import LandingPage from './pages/app/landing-page'
 
 export function App() {
   const { isAuthenticated } = useContext(authContext)
