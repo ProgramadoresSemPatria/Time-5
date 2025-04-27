0c7d5a47adb08a7d7dbfe4409343d140c311ca72
ramlow07 <luamramlow@gmail.com>
2025-04-27 10:34:03 -0300

    adding ProfilePage routes inside main

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { useContext } from 'react'
-import { authContext } from './context/auth-context'
-import { Dashboard } from './pages/app/dashboard'
-import { SignIn } from './pages/auth/sign-in'
-import { AppLayout } from './layout/app'
-import { SignUp } from './pages/auth/sign-up'
-import { LandingPage } from './pages/app/landing-page'
-import CVManager from './pages/app/cv-manager'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { authContext } from "./context/auth-context";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./layout/app";
+import { SignUp } from "./pages/auth/sign-up";
+import { LandingPage } from "./pages/app/landing-page";
+import CVManager from "./pages/app/cv-manager";
+import ProfilePage from "./pages/app/profile-page";
 
 export function App() {
-  const { isAuthenticated } = useContext(authContext)
+  const { isAuthenticated } = useContext(authContext);
 
   return (
     <BrowserRouter>
@@ -26,10 +27,13 @@ export function App() {
         <Route path="/cv-manager" element={isAuthenticated && <AppLayout />}>
           {isAuthenticated && <Route index element={<CVManager />} />}
         </Route>
+        <Route path="/profile-page" element={isAuthenticated && <AppLayout />}>
+          {isAuthenticated && <Route index element={<ProfilePage />} />}
+        </Route>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
