6ae78620a17690c17d03ca2379b10cd170c35bb0
Gustavo Bañares de Bacco <gustavobbacco@gmail.com>
2025-04-30 13:17:42 -0300

    feat: success page after the payment realized with success

---
+++ front/src/pages/app/sucess.tsx
@@ -0,0 +1,13 @@
+export default function Sucesso() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <h1 className="text-3xl font-bold text-green-600">
+        Pagamento realizado com sucesso!
+      </h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Obrigado pela sua compra. Em breve você receberá um e-mail com os
+        detalhes.
+      </p>
+    </div>
+  )
+}
